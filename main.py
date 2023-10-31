5626eda43794c8e85b0e6b15a8cc4d9af25d231a
josephcardillo1 <josephcardillo@ufl.edu>
2023-10-31 18:22:35 -0400

    I got it to work

--- main.py
+++ main.py
@@ -3,26 +3,33 @@
 def encode(password):
     int_list = ''
     for i in password:
-        x = int(i) + 3
-        int_list += str(x)
+        if int(i) < 7:
+            int_list += str(int(i) + 3)
+        else:
+            int_list += ((str(int(i) + 3))[1])
     return int_list
 
 
-
 def main():
-    while True:
-        print("Menu")
-        print('-------------')
-        print('1. Encode')
-        print('2. Decode')
-        print('3. Quit')
-        option = input('Please enter an option: ')
+    print("Menu")
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    option = input('Please enter an option: ')
 
-        if option == '1':
-            password = encode(input('Please enter your password to encode: '))
-            print('Your password has been encoded and stored! \n')
-            # print(password)
+    if option == '1':
+        old_password = input('Please enter your password to encode: ')
+        new_password = encode(old_password)
+        print('Your password has been encoded and stored! \n')
+        print(f'The encoded password is {new_password}, and the original password is {old_password}')
+
+    # elif option == '2':
+
+    elif option == '3':
+        return False
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
